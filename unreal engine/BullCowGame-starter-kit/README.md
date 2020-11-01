5a81fce76469957cbc7061f3f07e18b9f503a447
hessamg <hesam_g2002@yahoo.com>
2020-11-01 02:48:47 +0100

    add BullCowGame

---
+++ unreal engine/BullCowGame-starter-kit/README.md
@@ -0,0 +1,11 @@
+## BullCowGame
+# This is a word guessing game. 
+# Hidden words are pulled from a list.
+# The game will show you the number of lives.
+# The player will make a guess.
+# You will lose a life if you guess wrong.
+# The game will display the number of Bulls and Cows. 
+# Guess the correct letter in the wrong place and get a "Cow".
+# Guess the correct letter in the right place and get a "Bull".
+# If the player guesses the word correctly, the game is won.
+# If the lives run out, the game is over.
---
+++ unreal engine/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Console/Cartridge.h"
+#include "BullCowCartridge.generated.h"
+
+struct FBullCowCount
+{
+	int32 BullCount = 0;
+	int32 CowCount = 0;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class BULLCOWGAME_API UBullCowCartridge : public UCartridge
+{
+	GENERATED_BODY()
+
+	public:
+	virtual void BeginPlay() override;
+	virtual void OnInput(const FString& Input) override;
+	void SetupGame();
+	void EndGame();
+	void ProcessInput(const FString& Guess);
+	bool IsIsogram(const FString& Word) const;
+	TArray<FString> GetValidWords(const TArray<FString>& WordList) const;
+	FBullCowCount GetBullCows(const FString& Guess) const;
+
+	// Your declarations go below!
+	private:
+	FString HiddenWord;
+	int32 Lives;
+	bool bGameOver;
+	TArray<FString> Isograms;
+};
